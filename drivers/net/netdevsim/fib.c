3c0f479f6b305365a97b2c574eb58fa71bf84867
Amit Cohen <amcohen@nvidia.com>
2021-02-01 21:47:53 +0200

    BACKPORT: net: Pass 'net' struct as first argument to fib6_info_hw_flags_set()
    The next patch will emit notification when hardware flags are changed,
    in case that fib_notify_on_flag_change sysctl is set to 1.
    
    To know sysctl values, net struct is needed.
    This change is consistent with the IPv4 version, which gets 'net' struct
    as its first argument.
    
    Currently, the only callers of this function are mlxsw and netdevsim.
    Patch the callers to pass net.
    
    Change-Id: I3218b00599100d4ea16ab1356ce0ef83104b26b8
    Signed-off-by: Amit Cohen <amcohen@nvidia.com>
    Signed-off-by: Ido Schimmel <idosch@nvidia.com>
    Reviewed-by: David Ahern <dsahern@kernel.org>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/netdevsim/fib.c
+++ drivers/net/netdevsim/fib.c
@@ -581,13 +581,15 @@ err_fib6_rt_nh_del:
 	return err;
 }
 
-static void nsim_fib6_rt_hw_flags_set(const struct nsim_fib6_rt *fib6_rt,
+static void nsim_fib6_rt_hw_flags_set(struct nsim_fib_data *data,
+				      const struct nsim_fib6_rt *fib6_rt,
 				      bool trap)
 {
+	struct net *net = devlink_net(data->devlink);
 	struct nsim_fib6_rt_nh *fib6_rt_nh;
 
 	list_for_each_entry(fib6_rt_nh, &fib6_rt->nh_list, list)
-		fib6_info_hw_flags_set(fib6_rt_nh->rt, false, trap);
+		fib6_info_hw_flags_set(net, fib6_rt_nh->rt, false, trap);
 }
 
 static int nsim_fib6_rt_add(struct nsim_fib_data *data,
@@ -608,7 +610,7 @@ static int nsim_fib6_rt_add(struct nsim_fib_data *data,
 		goto err_fib_dismiss;
 	}
 
-	nsim_fib6_rt_hw_flags_set(fib6_rt, true);
+	nsim_fib6_rt_hw_flags_set(data, fib6_rt, true);
 
 	return 0;
 
@@ -634,9 +636,9 @@ static int nsim_fib6_rt_replace(struct nsim_fib_data *data,
 		return err;
 	}
 
-	nsim_fib6_rt_hw_flags_set(fib6_rt, true);
+	nsim_fib6_rt_hw_flags_set(data, fib6_rt, true);
 
-	nsim_fib6_rt_hw_flags_set(fib6_rt_old, false);
+	nsim_fib6_rt_hw_flags_set(data, fib6_rt_old, false);
 	nsim_fib6_rt_destroy(fib6_rt_old);
 
 	return 0;
@@ -796,7 +798,7 @@ static void nsim_fib6_rt_free(struct nsim_fib_rt *fib_rt,
 	struct nsim_fib6_rt *fib6_rt;
 
 	fib6_rt = container_of(fib_rt, struct nsim_fib6_rt, common);
-	nsim_fib6_rt_hw_flags_set(fib6_rt, false);
+	nsim_fib6_rt_hw_flags_set(data, fib6_rt, false);
 	nsim_fib_account(&data->ipv6.fib, false, NULL);
 	nsim_fib6_rt_destroy(fib6_rt);
 }
