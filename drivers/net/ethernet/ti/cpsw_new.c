bb313b7f7cb1ce22742ed1e8fdb56d5462dab957
Lorenzo Bianconi <lorenzo@kernel.org>
2021-03-08 12:06:58 +0100

    BACKPORT: bpf, devmap: Move drop error path to devmap for XDP_REDIRECT
    We want to change the current ndo_xdp_xmit drop semantics because it will
    allow us to implement better queue overflow handling. This is working
    towards the larger goal of a XDP TX queue-hook. Move XDP_REDIRECT error
    path handling from each XDP ethernet driver to devmap code. According to
    the new APIs, the driver running the ndo_xdp_xmit pointer, will break tx
    loop whenever the hw reports a tx error and it will just return to devmap
    caller the number of successfully transmitted frames. It will be devmap
    responsibility to free dropped frames.
    
    Move each XDP ndo_xdp_xmit capable driver to the new APIs:
    
    - veth
    - virtio-net
    - mvneta
    - mvpp2
    - socionext
    - amazon ena
    - bnxt
    - freescale (dpaa2, dpaa)
    - xen-frontend
    - qede
    - ice
    - igb
    - ixgbe
    - i40e
    - mlx5
    - ti (cpsw, cpsw-new)
    - tun
    - sfc
    
    Change-Id: I199e97ec71dc769c667f393c5a9f665dff3ee982
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Reviewed-by: Ioana Ciornei <ioana.ciornei@nxp.com>
    Reviewed-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>
    Reviewed-by: Camelia Groza <camelia.groza@nxp.com>
    Acked-by: Edward Cree <ecree.xilinx@gmail.com>
    Acked-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Acked-by: Shay Agroskin <shayagr@amazon.com>
    Link: https://lore.kernel.org/bpf/ed670de24f951cfd77590decf0229a0ad7fd12f6.1615201152.git.lorenzo@kernel.org
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- drivers/net/ethernet/ti/cpsw_new.c
+++ drivers/net/ethernet/ti/cpsw_new.c
@@ -1095,24 +1095,22 @@ static int cpsw_ndo_xdp_xmit(struct net_device *ndev, int n,
 {
 	struct cpsw_priv *priv = netdev_priv(ndev);
 	struct xdp_frame *xdpf;
-	int i, drops = 0;
+	int i, nxmit = 0;
 
 	if (unlikely(flags & ~XDP_XMIT_FLAGS_MASK))
 		return -EINVAL;
 
 	for (i = 0; i < n; i++) {
 		xdpf = frames[i];
-		if (xdpf->len < READ_ONCE(priv->tx_packet_min)) {
-			xdp_return_frame_rx_napi(xdpf);
-			drops++;
-			continue;
-		}
+		if (xdpf->len < READ_ONCE(priv->tx_packet_min))
+			break;
 
 		if (cpsw_xdp_tx_frame(priv, xdpf, NULL, priv->emac_port))
-			drops++;
+			break;
+		nxmit++;
 	}
 
-	return n - drops;
+	return nxmit;
 }
 
 static int cpsw_get_port_parent_id(struct net_device *ndev,
