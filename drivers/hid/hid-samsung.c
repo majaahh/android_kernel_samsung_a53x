e6b219b86123f44ddd55cd87df4bdfb385acff75
Sandeep C S <sandeep.cs@samsung.com>
2024-01-25 10:06:24 +0530

    UPSTREAM: HID: samsung: Broaden device compatibility in samsung driver
    The USB validation check has been moved wherever its required.
    
    Earlier Samsung driver only handles USB HID devices and returns an error if it
    encounters a Bluetooth type of HID device.  This changes improves driver
    compatibility and extends its support for a wide range of devices.
    
    Change-Id: I4862ea44b9272aa8ad413e6c81e1e4a318bfe48d
    Signed-off-by: Sandeep C S <sandeep.cs@samsung.com>
    Signed-off-by: Junwan Cho <junwan.cho@samsung.com>
    Signed-off-by: Jitender Sajwan <jitender.s21@samsung.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.com>
    (cherry picked from commit ac203cdbe97a197dddd2925c542ad5a52a8693fa)
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- drivers/hid/hid-samsung.c
+++ drivers/hid/hid-samsung.c
@@ -480,7 +480,7 @@ static int samsung_universal_kbd_input_mapping(struct hid_device *hdev,
 static __u8 *samsung_report_fixup(struct hid_device *hdev, __u8 *rdesc,
 	unsigned int *rsize)
 {
-	if (USB_DEVICE_ID_SAMSUNG_IR_REMOTE == hdev->product)
+	if (USB_DEVICE_ID_SAMSUNG_IR_REMOTE == hdev->product && hid_is_usb(hdev))
 		rdesc = samsung_irda_report_fixup(hdev, rdesc, rsize);
 	return rdesc;
 }
@@ -491,7 +491,7 @@ static int samsung_input_mapping(struct hid_device *hdev, struct hid_input *hi,
 {
 	int ret = 0;
 
-	if (USB_DEVICE_ID_SAMSUNG_WIRELESS_KBD_MOUSE == hdev->product)
+	if (USB_DEVICE_ID_SAMSUNG_WIRELESS_KBD_MOUSE == hdev->product && hid_is_usb(hdev))
 		ret = samsung_kbd_mouse_input_mapping(hdev,
 			hi, field, usage, bit, max);
 	else if (hdev->product == USB_DEVICE_ID_SAMSUNG_WIRELESS_KBD)
@@ -526,6 +526,10 @@ static int samsung_probe(struct hid_device *hdev,
 	}
 
 	if (USB_DEVICE_ID_SAMSUNG_IR_REMOTE == hdev->product) {
+		if (!hid_is_usb(hdev)) {
+			ret = -EINVAL;
+			goto err_free;
+		}
 		if (hdev->rsize == 184) {
 			/* disable hidinput, force hiddev */
 			cmask = (cmask & ~HID_CONNECT_HIDINPUT) |
