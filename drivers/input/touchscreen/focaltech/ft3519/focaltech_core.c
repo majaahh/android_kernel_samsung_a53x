9c3efb8a05095f545d1b8f794518068dd9df74cb
Flopster101 <nahuelgomez329@gmail.com>
2025-07-16 18:19:13 -0300

    input: touchscreen(fts, gdx, nvt): Snap proximity value to 5cm
    By default it reports 0cm and 1cm in this mode, and AOSP can still consider
    1cm as "near".
    
    Change-Id: Id3c71846040923a27578ef09d325aeefab722807
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- drivers/input/touchscreen/focaltech/ft3519/focaltech_core.c
+++ drivers/input/touchscreen/focaltech/ft3519/focaltech_core.c
@@ -475,7 +475,7 @@ void fts_release_all_finger(void)
 		input_sync(input_dev);
 	}
 
-	ts_data->hover_event = !ts_data->hover_event;
+	ts_data->hover_event = !ts_data->hover_event ? 5 : 0;
 
 	if (ts_data->pdata->input_dev_proximity) {
 		ts_data->pocket_state = ts_data->hover_event = 0xff;
@@ -838,7 +838,7 @@ static int fts_read_pocket_result(struct fts_ts_data *ts_data)
 		ts_data->hover_event = IN_POCKET;
 	}
 
-	ts_data->hover_event = !ts_data->hover_event;
+	ts_data->hover_event = !ts_data->hover_event ? 5 : 0;
 
 	input_report_abs(ts_data->pdata->input_dev_proximity, ABS_MT_CUSTOM, ts_data->hover_event);
 	input_sync(ts_data->pdata->input_dev_proximity);
@@ -863,7 +863,7 @@ static int fts_read_proximity_result(struct fts_ts_data *ts_data)
 	else
 		ts_data->hover_event = (val >> 4);
 
-	ts_data->hover_event = !ts_data->hover_event;
+	ts_data->hover_event = !ts_data->hover_event ? 5 : 0;
 
 	input_report_abs(ts_data->pdata->input_dev_proximity, ABS_MT_CUSTOM, ts_data->hover_event);
 	input_sync(ts_data->pdata->input_dev_proximity);
