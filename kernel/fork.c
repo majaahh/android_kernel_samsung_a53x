18428b5285f433c655af44da8aefe4ad43882c90
Flopster101 <nahuelgomez329@gmail.com>
2025-11-20 15:08:56 -0300

    treewide: Nuke KDP/RKP/UH
    Change-Id: I3bc895d22bc7e007060a2d0585eba6c173fab2a1
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- kernel/fork.c
+++ kernel/fork.c
@@ -116,10 +116,6 @@
 #ifdef CONFIG_SECURITY_DEFEX
 #include <linux/defex.h>
 #endif
-
-#ifdef CONFIG_KDP_CRED
-#include <linux/kdp.h>
-#endif
 /*
  * Minimum number of threads to boot the kernel
  */
@@ -2472,10 +2468,6 @@ static __latent_entropy struct task_struct *copy_process(
 
 	copy_oom_score_adj(clone_flags, p);
 
-#ifdef CONFIG_KDP_CRED
-	if (kdp_enable)
-		kdp_assign_pgd(p);
-#endif
 	return p;
 
 bad_fork_cancel_cgroup:
