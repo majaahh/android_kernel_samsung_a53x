64faddeac2a798b79c6e74f6609b00b3d355893b
dupazlasu <dupazlasu66@gmail.com>
2025-12-10 18:30:22 +0100

    fs: proc: proc_avc: backport from SM-S918B
    Change-Id: I0fdd1e14d7780c72dffd6c899c3a0a595e778fa5
    Co-authored-by: ExtremeXT <extremext@lineageos.org>
    Signed-off-by: dupazlasu <dupazlasu66@gmail.com>
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- kernel/audit.c
+++ kernel/audit.c
@@ -62,6 +62,9 @@
 
 #include "audit.h"
 
+#ifdef CONFIG_PROC_AVC
+#include <linux/proc_avc.h>
+#endif
 /* No auditing will take place until audit_initialized == AUDIT_INITIALIZED.
  * (Initialization happens after skb_init is called.) */
 #define AUDIT_DISABLED		-1
@@ -545,8 +548,15 @@ static void kauditd_printk_skb(struct sk_buff *skb)
 	struct nlmsghdr *nlh = nlmsg_hdr(skb);
 	char *data = nlmsg_data(nlh);
 
+// [ SEC_SELINUX_PORTING_COMMON
+#ifdef CONFIG_PROC_AVC
+	if (nlh->nlmsg_type != AUDIT_EOE && nlh->nlmsg_type != AUDIT_NETFILTER_CFG)
+		sec_avc_log("%s\n", data);
+#else
 	if (nlh->nlmsg_type != AUDIT_EOE && printk_ratelimit())
 		pr_notice("type=%d %s\n", nlh->nlmsg_type, data);
+#endif
+// ] SEC_SELINUX_PORTING_COMMON
 }
 
 /**
@@ -788,6 +798,15 @@ retry:
 			} else
 				goto retry;
 		} else {
+// [ SEC_SELINUX_PORTING_COMMON
+#ifdef CONFIG_PROC_AVC
+			struct nlmsghdr *nlh = nlmsg_hdr(skb);
+			char *data = nlmsg_data(nlh);
+
+			if (nlh->nlmsg_type != AUDIT_EOE && nlh->nlmsg_type != AUDIT_NETFILTER_CFG)
+				sec_avc_log("%s\n", data);
+#endif
+// ] SEC_SELINUX_PORTING_COMMON
 			/* skb sent - drop the extra reference and continue */
 			consume_skb(skb);
 			failed = 0;
