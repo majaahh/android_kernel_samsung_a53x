3898e6ea1e9d3c72614a3a89284d957876815970
Majaahh <maja.blaszczak@proton.me>
2025-12-17 15:55:48 +0100

    drivers: gpu: samsung/panel: Add extra 60HS_120HS_TE_HW_SKIP_1 node
    Change-Id: Ie6bb33c132f65f6fab0181b0e17943fcba1a3681
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- drivers/gpu/drm/samsung/panel/s6e3fc3/s6e3fc3_a53x_resol.h
+++ drivers/gpu/drm/samsung/panel/s6e3fc3/s6e3fc3_a53x_resol.h
@@ -25,6 +25,12 @@ struct panel_vrr s6e3fc3_a53x_default_panel_vrr[] = {
 		.te_hw_skip_count = 0,
 		.mode = VRR_HS_MODE,
 	},
+	[S6E3FC3_VRR_60HS_120HS_TE_HW_SKIP_1] = {
+		.fps = 120,
+		.te_sw_skip_count = 0,
+		.te_hw_skip_count = 1,
+		.mode = VRR_HS_MODE,
+	},
 	[S6E3FC3_VRR_60HS] = {
 		.fps = 60,
 		.te_sw_skip_count = 0,
@@ -35,6 +41,7 @@ struct panel_vrr s6e3fc3_a53x_default_panel_vrr[] = {
 
 static struct panel_vrr *s6e3fc3_a53x_default_vrrtbl[] = {
 	&s6e3fc3_a53x_default_panel_vrr[S6E3FC3_VRR_120HS],
+	&s6e3fc3_a53x_default_panel_vrr[S6E3FC3_VRR_60HS_120HS_TE_HW_SKIP_1],
 	&s6e3fc3_a53x_default_panel_vrr[S6E3FC3_VRR_60HS],
 };
 
@@ -62,6 +69,11 @@ static struct common_panel_display_mode s6e3fc3_a53x_display_mode[] = {
 		.resol = &s6e3fc3_a53x_default_resol[S6E3FC3_RESOL_1080x2400],
 		.vrr = &s6e3fc3_a53x_default_panel_vrr[S6E3FC3_VRR_120HS],
 	},
+	[S6E3FC3_A53X_DISPLAY_MODE_1080x2400_60HS_120HS_TE_HW_SKIP_1] = {
+		.name = PANEL_DISPLAY_MODE_1080x2400_60HS_120HS_TE_HW_SKIP_1,
+		.resol = &s6e3fc3_a53x_default_resol[S6E3FC3_RESOL_1080x2400],
+		.vrr = &s6e3fc3_a53x_default_panel_vrr[S6E3FC3_VRR_60HS_120HS_TE_HW_SKIP_1],
+	},
 	[S6E3FC3_A53X_DISPLAY_MODE_1080x2400_60HS] = {
 		.name = PANEL_DISPLAY_MODE_1080x2400_60HS,
 		.resol = &s6e3fc3_a53x_default_resol[S6E3FC3_RESOL_1080x2400],
@@ -71,6 +83,7 @@ static struct common_panel_display_mode s6e3fc3_a53x_display_mode[] = {
 
 static struct common_panel_display_mode *s6e3fc3_a53x_display_mode_array[] = {
 	[S6E3FC3_A53X_DISPLAY_MODE_1080x2400_120HS] = &s6e3fc3_a53x_display_mode[S6E3FC3_A53X_DISPLAY_MODE_1080x2400_120HS],
+	[S6E3FC3_A53X_DISPLAY_MODE_1080x2400_60HS_120HS_TE_HW_SKIP_1] = &s6e3fc3_a53x_display_mode[S6E3FC3_A53X_DISPLAY_MODE_1080x2400_60HS_120HS_TE_HW_SKIP_1],
 	[S6E3FC3_A53X_DISPLAY_MODE_1080x2400_60HS] = &s6e3fc3_a53x_display_mode[S6E3FC3_A53X_DISPLAY_MODE_1080x2400_60HS],
 };
 
