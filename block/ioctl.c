ecb8d9f36119e5820aa52f047549d058d816105e
Majaahh <maja.blaszczak@proton.me>
2026-04-09 15:27:26 +0200

    block: Debloat
    Change-Id: I63e7976e235e4d414d79799ba859924eb272b8e1
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- block/ioctl.c
+++ block/ioctl.c
@@ -145,6 +145,7 @@ static int blk_ioctl_discard(struct block_device *bdev, fmode_t mode,
 
 	if (start + len > i_size_read(bdev->bd_inode))
 		return -EINVAL;
+
 	printk("%s %d:%d %llu %llu",
 		(flags & BLKDEV_DISCARD_SECURE) ? "SECDIS" : "DIS",
 		MAJOR(bdev->bd_dev), MINOR(bdev->bd_dev),
