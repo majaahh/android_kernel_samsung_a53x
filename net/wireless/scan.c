98a7ca39678275f5cc90b6c49fba32dd63b15554
Majaahh <maja.blaszczak@proton.me>
2026-04-09 15:47:30 +0200

    net: Import debloated changes from A256U1UES9DYK3
    Change-Id: Iede72aa5ba090ee945b3ad8a7c4b3c9c7e6ef323
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- net/wireless/scan.c
+++ net/wireless/scan.c
@@ -1773,10 +1773,8 @@ cfg80211_update_known_bss(struct cfg80211_registered_device *rdev,
 		/* Override IEs if they were from a beacon before */
 		if (old == rcu_access_pointer(known->pub.ies))
 			rcu_assign_pointer(known->pub.ies, new->pub.beacon_ies);
-
-		cfg80211_update_hidden_bsses(known,
-					     rcu_access_pointer(new->pub.beacon_ies),
-					     old);
+		
+		cfg80211_update_hidden_bsses(known, new->pub.beacon_ies, old);
 
 		if (old)
 			kfree_rcu((struct cfg80211_bss_ies *)old, rcu_head);
